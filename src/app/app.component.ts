fcde538a891874b2ea077c89c91318945e306f3b
domagoj954 <domagoj954@gmail.com>
2024-03-25 16:27:21 +0100

    Appointment list component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AppointmentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
